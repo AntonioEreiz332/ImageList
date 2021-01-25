49faf59496cc2fc6967634f3326fd01a9ac21812
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-01-25 14:18:57 +0100

    Add files via upload

---
+++ imagelist zad3/imagelist zad3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace imagelist_zad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPromijeni = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPromijeni
+            // 
+            this.btnPromijeni.Location = new System.Drawing.Point(13, 477);
+            this.btnPromijeni.Name = "btnPromijeni";
+            this.btnPromijeni.Size = new System.Drawing.Size(106, 23);
+            this.btnPromijeni.TabIndex = 0;
+            this.btnPromijeni.Text = "Promijeni sliku ";
+            this.btnPromijeni.UseVisualStyleBackColor = true;
+            this.btnPromijeni.Click += new System.EventHandler(this.btnPromijeni_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(67, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(463, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Takumi1.jpg");
+            this.imageList1.Images.SetKeyName(1, "Itsuki.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPromijeni);
+            this.Name = "Form1";
+            this.Text = "Upotreba ImageList komponente";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPromijeni;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
